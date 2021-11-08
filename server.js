a160503a11d77826e50926c7ab0c1311a21107d1
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2021-11-08 13:50:02 +0100

    Added CORS

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const satelliteRoutes = require('./routes/satellite');
 const planetRoutes = require('./routes/planets');
+
+// Remember: The order of your middleware is very important!
+
+// We use CORS() library to insert CORS related headers into the request object
+app.use(cors());
 
 app.use("/satellite", satelliteRoutes);
 app.use("/planets", planetRoutes);
